f04e17598fe3aa4032c22e86f78357a5dd50ae5a
Scott Volk <scottvolk@gmail.com>
2018-11-13 09:19:57 -0800

    Ran pid.py through a Linter

--- pid.py
+++ pid.py
@@ -5,27 +5,27 @@ class PID(object):
   INTEGRATOR_CLAMP     = 8
   DIFFERENTIATOR_CLAMP = 8
 
-  def __init__(self,kp,ki,kd,*args,**kwargs):
+  def __init__(self, kp, ki, kd, *args, **kwargs):
     self.kp         = kp
     self.ki         = ki
     self.kd         = kd
-    self.error      = [0,0]
+    self.error      = [0, 0]
     self.clamp_p    = PID.PROPORTIONAL_CLAMP
     self.clamp_i    = PID.INTEGRATOR_CLAMP
     self.clamp_d    = PID.DIFFERENTIATOR_CLAMP
     self.integrator = 0
 
   def reset(self):
-    self.error      = [0,0]
+    self.error      = [0, 0]
     self.integrator = 0
 
-  def feedback(self,error):
+  def feedback(self, error):
     result         = 0
     proportional   = 0
     differentiator = 0
     sign           = 0
 
-    self.error[0] = error;
+    self.error[0] = error
 
     proportional = error * self.kp
     sign         = np.sign(proportional)
@@ -42,7 +42,7 @@ class PID(object):
     differentiator  = abs(differentiator) if abs(differentiator) < self.clamp_d else self.clamp_d
     differentiator *= sign
 
-    self.error[1] = error;
+    self.error[1] = error
 
     result  = proportional
     result += self.integrator
